7b4c3eb9c090b51767bacf338af0a32416442fac
SourabhJangid <sourabhjangid1558@gmail.com>
2021-12-06 17:39:05 +0530

    Removed unecessary import statements

--- src/main/java/com/qjob/cricket/controller/TeamController.java
+++ src/main/java/com/qjob/cricket/controller/TeamController.java
@@ -4,8 +4,6 @@ import com.qjob.cricket.Model.Team;
 import com.qjob.cricket.repository.MatchRepository;
 import com.qjob.cricket.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
