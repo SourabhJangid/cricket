a9ccd36e0d56b6849364ea3b9d4b546aa7c5bc16
SourabhJangid <sourabhjangid1558@gmail.com>
2021-12-08 19:12:34 +0530

    added basic ui components

---
+++ src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+export const MatchDetailCard = () => {
+  return (
+    <div className="MatchDetailCard">
+        <h2>Latest Match</h2>
+    </div>
+  );
+}
+
+export default MatchDetailCard;
---
+++ src/frontend/src/components/MatchSmallCard.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+export const MatchSmallCard = () => {
+  return (
+    <div className="MatchSmallCard">
+        <h3>Other Match</h3>
+    </div>
+  );
+}
+
+export default MatchSmallCard;
