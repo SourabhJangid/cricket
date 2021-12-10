61ff710a21ccb96cd2a075e50395446f396726cc
SourabhJangid <sourabhjangid1558@gmail.com>
2021-12-11 00:30:21 +0530

    Created Matches Api and added that part in frontend

--- src/main/java/com/qjob/cricket/controller/TeamController.java
+++ src/main/java/com/qjob/cricket/controller/TeamController.java
@@ -1,13 +1,14 @@
 package com.qjob.cricket.controller;
 
+import com.qjob.cricket.Model.Match;
 import com.qjob.cricket.Model.Team;
 import com.qjob.cricket.repository.MatchRepository;
 import com.qjob.cricket.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -24,6 +25,15 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return matchRepository.getByTeamNameAndDate(
+                teamName, startDate, endDate
+        );
     }
 
 }
